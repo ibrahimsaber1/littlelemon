df60a5e6b860c7ea9ac4c6bf981ea155d895df59
Ibrahim Saber <ibrahimsaber622@gmail.com>
2025-12-13 23:17:09 +0200

    add: model serializer to change the inventory into stock

--- crudapis/serializers.py
+++ crudapis/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import MenuItem
 
-class MenuItemSerializer(serializers.ModelSerializer):
+
+# 1- serializers
+class menuItemSerializers(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    price = serializers.DecimalField(max_digits=8, decimal_places=2)
+    inventory = serializers.IntegerField()
+
+# 2- ModelSerializer
+class MenuItemModelSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source='inventory')
     class Meta:
         model = MenuItem
-        fields = '__all__'
+        fields = ['id', 'title','price','stock']
