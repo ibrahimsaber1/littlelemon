e86d408f2cf3e6b735744a0040f9347b41ece77c
Ibrahim Saber <ibrahimsaber622@gmail.com>
2025-12-13 23:37:56 +0200

    add: price_after_tax field to the serializer

--- crudapis/serializers.py
+++ crudapis/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import MenuItem
-
+from decimal import Decimal
 
 # 1- serializers
 class menuItemSerializers(serializers.Serializer):
@@ -12,6 +12,11 @@ class menuItemSerializers(serializers.Serializer):
 # 2- ModelSerializer
 class MenuItemModelSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
+    price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
+
     class Meta:
         model = MenuItem
-        fields = ['id', 'title','price','stock']
+        fields = ['id', 'title','price','stock','price_after_tax']
+        
+    def calculate_tax(self, product:MenuItem):
+        return product.price * Decimal(1.1)
